5a4247688cd22ac61d02003d5832102dd6d8e7e7
Fabio-Bianco <biancobfabio@gmail.com>
2025-08-21 12:08:22 +0200

    Correggi la dichiarazione della variabile $eta e rimuovi il codice PHP duplicato nel file index.php

--- index.php
+++ index.php
@@ -26,6 +26,46 @@ echo "Il mio nome è " . $name .  " e ho " . $eta . " anni." ;
 $eta = 35
 
 ?>
+
+<hr>
+
+<?php
+echo "Questo è un altro blocco di codice PHP con le istruzioni condizionali";
+
+$numero = 20;
+
+if($numero > 10) {
+    echo "<br>Il numero è maggiore di 10";
+} else {
+    echo "<br>Il numero è minore o uguale a 10";
+}
+
+?>
+
+<hr>
+
+<?php
+
+echo "Questo è un altro blocco di codice PHP con array";    
+
+$numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+echo "<h3>var_dump()</h3>";
+echo "<pre>";
+var_dump($numbers);
+echo "</pre>";
+
+echo "<h3>print_r()</h3>";
+echo "<pre>";
+print_r($numbers);
+echo "</pre>";
+
+echo "<h3>var_export()</h3>";
+echo "<pre>";
+var_export($numbers);
+echo "</pre>";
+
+?>
     
 </body>
 </html>
